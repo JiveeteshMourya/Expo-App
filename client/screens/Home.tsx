b3f69952f3a5a5fa582de771de7cbf1288672220
Jiveetesh Mourya <jcodestudy@gmail.com>
2025-06-11 16:39:39 +0530

    footer menu

--- client/screens/Home.tsx
+++ client/screens/Home.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useContext } from 'react'
 import { AuthContext } from '@/context/authContext'
+import FooterMenu from '@/components/Menus/FooterMenu';
 
 const Home = () => {
     // global state
@@ -10,13 +11,20 @@ const Home = () => {
     }
     const { state } = context;
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home</Text>
       <Text>{JSON.stringify(state, null, 4)}</Text>
+      <FooterMenu/>
     </View>
   )
 }
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: "space-around",
+    padding: 10,
+  }
+});
